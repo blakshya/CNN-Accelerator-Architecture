925bcd3d22a17265cfc8814a6ea578a222bb90d4
lakshya <bhatnagarlakshya3@gmail.com>
2020-07-22 23:49:41 +0530

    Pooling ALU Test
    looks ok. need to check Pooling Unit Now

--- src/design/PoolingALU.v
+++ src/design/PoolingALU.v
@@ -1,9 +1,7 @@
 `timescale 1ns / 1ps
 
 module PoolingALU #(parameter
-    depth = 2,
-    D = 1<<depth,
-    W = 16
+        W = 16
     )(
         input wire [3:0] controlSignal,
         input  wire [W-1:0] ip,
@@ -13,14 +11,14 @@ module PoolingALU #(parameter
         output reg  [W-1:0] max,
         input  wire CLK
     );
-/* DESCRIPTION OF THE control (ctl)
+/* DESCRIPTION OF controlSignal
     * [MSB:LSM] - write, useUpper, useCurrent, useLower
-    * write - 1: write to max, 0:dont write to max
+    * write        - 1: write to max, 0:dont write to max
     * useUpper     - 1: use input from unit above, 0: replace with zero
     * useCurrent   - 1: use input from buffer    , 0: replace with zero
     * useLower     - 1: use input from unit below, 0: replace with zero
 */
-// parameter 
+
     wire write,useUpper,useCurrent,useLower;
         assign {write,useUpper,useCurrent,useLower} = controlSignal;
 
@@ -37,10 +35,12 @@ module PoolingALU #(parameter
 
     assign op = max2;
 
+    wire [W-1:0] max3, sub3;
+        assign sub3 = max - max2;
+        assign max3 = sub3[W-1]? max2:max;
+
     always @(negedge CLK) begin
-        if (write) begin
-            max <= op;
-        end
+        max <= write?((useUpper||useCurrent||useLower)?max3:0):max;
     end
 
 endmodule
